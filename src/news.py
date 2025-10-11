91e54f0515c99b7f90a837f244ae9a6e0fac3921
OliverAnderson <oliverandersonllorens18@gmail.com>
2025-10-11 21:42:05 +0200

    src.news done

---
+++ src/news.py
@@ -0,0 +1,21 @@
+import requests as r
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+apiKey = os.getenv("NEWSAPI_KEY")
+
+def get_news(asset: str) -> pd.DataFrame:
+    url = "https://newsapi.org/v2/everything"
+    params = {
+        "q": asset,
+        "apiKey": apiKey
+    }
+    
+    response = r.get(url, params=params)
+    data = response.json() 
+    
+    df = pd.DataFrame(data["articles"])[["publishedAt", "title", "description", "content"]]
+    return df
